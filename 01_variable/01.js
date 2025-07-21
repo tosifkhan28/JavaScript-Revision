d45faf5dbade2e70e56bbdedc8bf182b2e4781b2
Mohammad Tosif Khan <162069722+tosifkhan28@users.noreply.github.com>
2025-07-21 11:11:57 +0000

    revision of variable

---
+++ 01_variable/01.js
@@ -0,0 +1,14 @@
+const accountId = 47854
+let accountEmail = "mdtosif387@gmail.com"
+var accountPAss = "90909090"
+accountCity = "indore"
+
+
+let accountState;
+
+
+console.log(accountId);
+accountEmail = "mohammadtosif115@gmail.coom"
+accountPAss = "Pass@1234"
+accountCity = "dewash"
+console.table([accountId, accountEmail, accountPAss, accountCity, accountState]);
