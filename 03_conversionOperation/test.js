2534672c270eb3d3e7554a27cbcf62b4acdff8c7
Mohammad Tosif Khan <162069722+tosifkhan28@users.noreply.github.com>
2025-08-07 20:25:02 +0000

    add data dype

---
+++ 03_conversionOperation/test.js
@@ -0,0 +1,66 @@
+let score = "soumiya" // string data type
+
+//console.log(typeof score); // OP =>  string data type
+//console.log(typeof(score));  // OP => srting data type
+
+let valueInNumber = Number(score)
+// console.log(typeof valueInNumber); // OP string data type
+// console.log(valueInNumber); // op string data type
+
+
+// "33" => 33
+// "33abc" => NaN
+//  true => 1; false => 0
+
+let isLoggedIn = "suresh"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+//  console.log(booleanIsLoggedIn); // OP String Data Type
+
+// 1 => true; 0 => false
+// "" => false
+// "hitesh" => true
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+//  console.log(stringNumber); // OP string data type
+// console.log(typeof stringNumber); // OP String data Typr
+// 
+
+// *********************** Operations ***********************
+
+let value = 3
+let negValue = -value
+// console.log(negValue); OP will be show in negative "-3"
+
+// console.log(2+2); // 4
+// console.log(2-2); //0
+// console.log(2 * 2); //4
+// console.log(2 ** 3); //8
+// console.log(2 / 3); // 0.6666666666666666
+// console.log(2 % 3); // 2
+
+let str1 = "hello"
+let str2 = "Thor"
+
+let str3 = str1 + str2
+// console.log(str3); // hello thor
+
+// console.log("1" + 2); // 122
+// console.log(1 + "2"); // 122
+// console.log("1" + 2 + 2); //122
+// console.log(1 + 2 + "2"); //32
+
+// console.log((3 + 4) * 5 % 3); //2
+
+// console.log(+true); //1
+// console.log(+""); //0
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2 + 2
+
+let gameCounter = 100
+++gameCounter;
+// console.log(gameCounter); // 101
